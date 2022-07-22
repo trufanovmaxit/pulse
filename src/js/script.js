3fa5724aefc04ab85349ee3675b5e112a1561839
Max <trufanovmaxit@gmail.com>
2022-07-22 19:00:22 +0300

    animations

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,76 @@ $(document).ready(function(){
                 $('.overlay, #order').fadeIn('slow');
             });
         });
+        
+        function valideForms(form){
+            $(form).validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
+                    },
+                    phone: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Пожалуйста, введите свое имя",
+                        minlength: jQuery.validator.format("Введите {0} символа!")
+                    },
+                    phone: "Пожалуйста, введите свой номеер телефона",
+                    email: {
+                        required: "Пожалуйста, введите свою почту",
+                        email: "Неправильно введен адрес почты"
+                    }
+                }
+            });
+        };
+
+        valideForms('#consultation-form');
+        valideForms('#consultation form');
+        valideForms('#order form');
+
+        $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+        $('form').submit(function(e) {
+            e.preventDefault();
+
+            if (!$(this).valid()) {
+                return;
+            }
+
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn('slow');
+
+                $('form').trigger('reset');
+            });
+            return false;
+        });
+
+        // Smooth scroll and pageup
+
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 1600) {
+                $('.pageup').fadeIn();
+            } else {
+                $('.pageup').fadeOut();
+            }
+        });
+
+        $("a[href^='#']").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+        });
+
+        new WOW().init();
 });
